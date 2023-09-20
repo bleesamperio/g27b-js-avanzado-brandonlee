354fe5f0bc5fdce1483c61234db8376157545ce5
bleesamperio <bleesamperio@outlook.com>
2023-09-19 21:19:47 -0600

    Se agregan Async Await y Promises

--- 3-asincronia/index.js
+++ 3-asincronia/index.js
@@ -1,11 +1,15 @@
 import Sincronia from "./3.1-sincronia.js";
 import Asincronia from "./3.2-asincronia.js";
 import Callback from "./3.2.1-callback.js";
+import Promises from "./3.2.2-promises.js";
+import Await from "./3.2.3-Async-await.js";
 
 console.log("clase 2")
 
 Sincronia.ejecucion()
 Asincronia.ejecucion()
 Callback.ejecucion()
+Promises.ejecucion()
+Await.ejecucion()
 
 console.log ("__________");
