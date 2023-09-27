20dd44773290f619d24b89b15175944783e8b03f
bleesamperio <bleesamperio@outlook.com>
2023-09-26 21:30:39 -0600

    Arreglo de languages & currencies

--- ejemplos-api/buscador-paises/script.js
+++ ejemplos-api/buscador-paises/script.js
@@ -5,7 +5,33 @@ const countryList = document.getElementById("country-list")
 function searchCountries(country){
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => {console.log(data)})
+    .then(data => {//Recibir de lista de paises
+        if(data.status =='404'){
+            countryList.innerHTML = "<p>Country not found</p>"
+        }else{
+            countryList.innerHTML = ''
+            data.forEach(country => {
+                const countryCard = document.createElement('div')
+                const currencies = Object.keys(country.currencies)//EUR
+                const name_currenciess = currencies.map(key => country.currencies[key].name).join()
+                const languages = Object.values(country.languages).join("")
+
+                console.log(name_currenciess)
+                console.log(languages)
+
+                countryCard.innerHTML = `
+                <h2>${country.name.common}</h2>
+                <h3>${country.name.official}</h3>
+                <p>Capital: ${country.capital.toString()}</p>
+                <p>Currencies: ${name_currenciess}</p>
+                <p>Languages: ${languages}</p>
+                <img src='${country.flag.png}'height=130px width=160px/> 
+
+                ` //Cierre de texto concatenado
+                countryList.appendChild(countryCard)
+            });
+        }
+    })
 }
 
 
